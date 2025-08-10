619892b4013a07d801df33997b80e18c2ffb1c48
hyen <hi020514@gmail.com>
2025-08-10 15:22:32 +0900

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/tableorder/TableOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.tableorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TableOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tableorder/TableOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tableorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
