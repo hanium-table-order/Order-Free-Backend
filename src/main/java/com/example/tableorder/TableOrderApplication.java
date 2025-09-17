8e02d735f26a25c19cc47e6e1e382b7d9990a3a1
HyoDong Jo <whgyehdjhd@naver.com>
2025-09-17 10:04:28 +0900

    fix:Swagger 서버 url 수정

--- src/main/java/com/example/tableorder/TableOrderApplication.java
+++ src/main/java/com/example/tableorder/TableOrderApplication.java
@@ -6,13 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@OpenAPIDefinition(
-        servers = {
-                @Server(url = "http://localhost:8080", description = "Local Server"),
-                @Server(url = "http://52.79.67.165:8080", description = "EC2 API Server")
-        }
-)
-
 @SpringBootApplication
 @EnableScheduling
 public class TableOrderApplication {
