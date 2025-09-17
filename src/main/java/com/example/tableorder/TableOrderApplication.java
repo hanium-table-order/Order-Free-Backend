ce513397c7e78c6773dcf09025727ea2a6a3e104
HyoDong Jo <whgyehdjhd@naver.com>
2025-09-17 09:27:10 +0900

    feat: Swagger 서버 URL 명시 추가 (EC2 8080 포트 설정)

--- src/main/java/com/example/tableorder/TableOrderApplication.java
+++ src/main/java/com/example/tableorder/TableOrderApplication.java
@@ -1,8 +1,17 @@
 package com.example.tableorder;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "http://localhost:8080", description = "Local Server"),
+                @Server(url = "http://52.79.67.165:8080", description = "EC2 API Server")
+        }
+)
 
 @SpringBootApplication
 @EnableScheduling
