24b5ecf72402b665ba05f797f8c628805a71f88f
hyodongg <whgyehdjhd@naver.com>
2025-10-02 11:48:47 +0900

    feat: 고객 주문 호출 기능 개발

--- src/main/java/com/example/tableorder/repository/StaffCallRepository.java
+++ src/main/java/com/example/tableorder/repository/StaffCallRepository.java
@@ -1,7 +1,16 @@
 package com.example.tableorder.repository;
 
 import com.example.tableorder.entity.staffcall.StaffCall;
+import com.example.tableorder.entity.store.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StaffCallRepository extends JpaRepository<StaffCall,Long> {
+    
+    // 특정 매장의 모든 직원 호출 조회
+    List<StaffCall> findByTable_Store(Store store);
+    
+    // 특정 테이블의 직원 호출 조회
+    List<StaffCall> findByTable_Id(Long tableId);
 }
